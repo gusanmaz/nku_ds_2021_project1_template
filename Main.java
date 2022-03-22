44716ec256b05fa6b4d51210c2ae551a310737fc
Güvenç Usanmaz <gusanmaz@gmail.com>
2022-03-22 11:14:44 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,48 @@
+public class Main {
+    public static void main(String args[]){
+       //main fonksiyonunu bu sekilde kalabilir. Bu fonksiyon altina yazdiklariniz degerlendirmeye alinmayacaktir.
+       System.out.println("Hello World");
+    }
+    
+    public static double TersToplam(int m, int n){
+      return 0;
+    }
+    
+    public static int AsalFark(int n){
+       return 0;
+    }
+    
+    public static int ToplaPalindrome5(){
+       return 0;
+    }
+    
+    public static int ToplaPalindrome6(){
+       return 0;
+    }
+    
+    public static double PiKareBul(int m){
+       return 0;
+    }
+    
+    public static double EulerBul(int m){
+       return 0;
+    }
+    
+    public static int KatalanSayisi(int n){
+       return 0;
+    }
+    
+    public static int PerrinSayisi(int n){
+       return 0;
+    }
+    
+    public static int CarpanToplam(int sayi){
+       return 0;
+    }
+    
+    public static int EkokBul(int m, int n){
+       return 0;
+    }
+    
+}
+
